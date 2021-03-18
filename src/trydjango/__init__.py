fde36c74c3b99137b3326f845767f1c830ad8346
AndrewBall79 <andy.bob.ball@gmail.com>
2021-03-18 13:27:09 -0500

    commit

---
+++ src/trydjango/__init__.py
@@ -0,0 +1,7 @@
+# echo "# Django" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/AndrewBall79/Django.git
+# git push -u origin main
